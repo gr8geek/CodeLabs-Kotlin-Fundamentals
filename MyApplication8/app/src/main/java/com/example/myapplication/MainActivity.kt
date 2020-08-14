b40a1127bae30b0fad8a14cc63348abbce930f95
gr8geek <pratyush9000@gmail.com>
2020-08-14 22:53:51 +0530

    Completed the dice roller app

--- MyApplication8/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication8/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,7 +20,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val ResultText : TextView = findViewById(R.id.result)
-        ResultText.text = ((1..6).random()).toString()
+        var randomInt: Int = (1..6).random()
+        ResultText.text = (randomInt).toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
     }
